0427420ef563eac93ad2f669dfea40a88d707d52
Weihua Fan <fanweihua@gmail.com>
2020-08-19 20:09:39 +0800

    Add samples for text/image/audio/video check API

---
+++ audiocheck_submit/main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"time"
+)
+
+const (
+	projectID             = "YOUR_PROJECT_ID_GOES_HERE"
+	secrectKey            = "YOUR_SECRET_KEY_GOES_HERE"
+	endpointURL           = "https://asafe.ilivedata.com/api/v1/audio/check/submit"
+	endpointHost          = "asafe.ilivedata.com"
+	endpointPath          = "/api/v1/audio/check/submit"
+	iso8601DateFormatNoMS = "2006-01-02T15:04:05Z"
+)
+
+func check(audioUrl string, type int, lang string, userID string) string {
+	// UTC Time
+	var now = time.Now().UTC().Format(iso8601DateFormatNoMS)
+	// Prepare parameters
+	var parameters = map[string]interface{}{
+		"type":  type,
+		"lang": lang,
+		"audio":  audioUrl,
+		"userId": userID,
+	}
+	var queryBody []byte
+	queryBody, err := json.Marshal(parameters)
+	if err != nil {
+		// log something
+		return err.Error()
+	}
+	//fmt.Println(string(queryBody))
+
+	var preparedString = []string{
+		"POST",
+		endpointHost,
+		endpointPath,
+		sha256AndHexEncode(string(queryBody)),
+		"X-AppId:" + projectID,
+		"X-TimeStamp:" + now,
+	}
+	var stringToSign = strings.Join(preparedString, "\n")
+	fmt.Println(stringToSign)
+	// Compute signature
+	var signature = signAndBase64Encode(stringToSign, secrectKey)
+	fmt.Println(signature)
+	// Send request
+	return request(string(queryBody), signature, now)
+}
+
+func signAndBase64Encode(data string, secrectKey string) string {
+	var mac = hmac.New(sha256.New, []byte(secrectKey))
+	mac.Write([]byte(data))
+	return base64.StdEncoding.EncodeToString(mac.Sum(nil))
+}
+
+func sha256AndHexEncode(data string) string {
+	var sha256Hash = sha256.New()
+	sha256Hash.Write([]byte(data))
+	return hex.EncodeToString(sha256Hash.Sum(nil))
+}
+
+func request(body string, signature string, timeStamp string) string {
+	var httpClient = http.Client{}
+	request, _ := http.NewRequest("POST", endpointURL, strings.NewReader(body))
+	request.Header.Set("X-AppId", projectID)
+	request.Header.Set("X-TimeStamp", timeStamp)
+	request.Header.Set("Authorization", signature)
+	request.Header.Set("Content-Type", "application/json")
+	request.Header.Set("User-Agent", "Golang_HTTP_Client/1.0")
+
+	response, err := httpClient.Do(request)
+
+	if err != nil {
+		// log something
+		return err.Error()
+	}
+	fmt.Println(response.StatusCode)
+	defer response.Body.Close()
+	content, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		// log something
+		return err.Error()
+	}
+	fmt.Println(string(content))
+	return string(content)
+}
+
+func main() {
+	var audioUrl = "https://speech-test.s3.cn-northwest-1.amazonaws.com.cn/speech/79b31026-d30a-11ea-aaa9-ac1f6b9346d0.wav"
+	check(audioUrl, 1, "zh-CN", "12345678")
+}
---
+++ videocheck_submit/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"time"
+)
+
+const (
+	projectID             = "YOUR_PROJECT_ID_GOES_HERE"
+	secrectKey            = "YOUR_SECRET_KEY_GOES_HERE"
+	endpointURL           = "https://vsafe.ilivedata.com/api/v1/video/check/submit"
+	endpointHost          = "vsafe.ilivedata.com"
+	endpointPath          = "/api/v1/video/check/submit"
+	iso8601DateFormatNoMS = "2006-01-02T15:04:05Z"
+)
+
+func check(videoUrl string, type int, userID string) string {
+	// UTC Time
+	var now = time.Now().UTC().Format(iso8601DateFormatNoMS)
+	// Prepare parameters
+	var parameters = map[string]interface{}{
+		"type":  type,
+		"video":  videoUrl,
+		"userId": userID,
+	}
+	var queryBody []byte
+	queryBody, err := json.Marshal(parameters)
+	if err != nil {
+		// log something
+		return err.Error()
+	}
+	//fmt.Println(string(queryBody))
+
+	var preparedString = []string{
+		"POST",
+		endpointHost,
+		endpointPath,
+		sha256AndHexEncode(string(queryBody)),
+		"X-AppId:" + projectID,
+		"X-TimeStamp:" + now,
+	}
+	var stringToSign = strings.Join(preparedString, "\n")
+	fmt.Println(stringToSign)
+	// Compute signature
+	var signature = signAndBase64Encode(stringToSign, secrectKey)
+	fmt.Println(signature)
+	// Send request
+	return request(string(queryBody), signature, now)
+}
+
+func signAndBase64Encode(data string, secrectKey string) string {
+	var mac = hmac.New(sha256.New, []byte(secrectKey))
+	mac.Write([]byte(data))
+	return base64.StdEncoding.EncodeToString(mac.Sum(nil))
+}
+
+func sha256AndHexEncode(data string) string {
+	var sha256Hash = sha256.New()
+	sha256Hash.Write([]byte(data))
+	return hex.EncodeToString(sha256Hash.Sum(nil))
+}
+
+func request(body string, signature string, timeStamp string) string {
+	var httpClient = http.Client{}
+	request, _ := http.NewRequest("POST", endpointURL, strings.NewReader(body))
+	request.Header.Set("X-AppId", projectID)
+	request.Header.Set("X-TimeStamp", timeStamp)
+	request.Header.Set("Authorization", signature)
+	request.Header.Set("Content-Type", "application/json")
+	request.Header.Set("User-Agent", "Golang_HTTP_Client/1.0")
+
+	response, err := httpClient.Do(request)
+
+	if err != nil {
+		// log something
+		return err.Error()
+	}
+	fmt.Println(response.StatusCode)
+	defer response.Body.Close()
+	content, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		// log something
+		return err.Error()
+	}
+	fmt.Println(string(content))
+	return string(content)
+}
+
+func main() {
+	var videoUrl = "http://edge.ivideo.sina.com.cn/34463529803.mp4?KID=sina,viask&Expires=1597852800&ssig=6po29lqbTn&reqid="
+	check(videoUrl, 1, "12345678")
+}
