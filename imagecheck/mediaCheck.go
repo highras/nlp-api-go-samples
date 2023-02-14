770175083a70f0a37090a440e7bbc2b384fae13d
jianjun.zhao <37098112+jianjunzhao-github@users.noreply.github.com>
2023-02-14 22:57:12 +0800

    Update mediaCheck.go

--- imagecheck/mediaCheck.go
+++ imagecheck/mediaCheck.go
@@ -15,21 +15,21 @@ import (
 const (
 	companyID            = "123"
 	secretKey            = "xxxxxxxxxxxxxx"
-    endpointURL          = "https://media-safe.ilivedata.com/service/check"
+	endpointURL          = "https://media-safe.ilivedata.com/service/check"
 	textAppID            = "80700001"
 	imageAppID            = "81000001"
 )
 
 func GetMD5Hash(text string) string {
-    hash := md5.Sum([]byte(text))
-    return hex.EncodeToString(hash[:])
+	hash := md5.Sum([]byte(text))
+	return hex.EncodeToString(hash[:])
 }
 
 func check(text string, imageList []string, userID string) string {
 
-    var timestamp = time.Now().Unix()
-    var hash = GetMD5Hash(companyID + ":" + strconv.Itoa(int(timestamp)) + ":" + secretKey)
-    var signature = strings.ToUpper(hash)
+	var timestamp = time.Now().Unix()
+	var hash = GetMD5Hash(companyID + ":" + strconv.Itoa(int(timestamp)) + ":" + secretKey)
+	var signature = strings.ToUpper(hash)
 
 	// Prepare parameters
 	var parameters = map[string]interface{}{
@@ -77,7 +77,7 @@ func request(body string) string {
 }
 
 func main() {
-    var imageList = [...]string {"https://test.com/1.jpg", "https://test/2.jpg"}
-    var responseJson = check("fuck test", imageList[:], "12345678")
+	var imageList = [...]string {"https://test.com/1.jpg", "https://test/2.jpg"}
+	var responseJson = check("fuck test", imageList[:], "12345678")
 	fmt.Println(responseJson)
 }
